5c85365edefb8d0e9fabd444d206eb1bc2aea3bb
ipcjs <gipcjs@gmail.com>
2023-06-04 04:58:48 +0800

    add rime

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 将所有微信表情导出成如下字典文件:
 
-- GBoard(全拼): [wechat-emoji-gboard-pinyin.zip](https://github.com/ipcjs/wechat-emoji-dict/releases/download/v1.0.0/wechat-emoji-gboard-pinyin.zip)
-- GBoard(小鹤双拼): [wechat-emoji-gboard-flypy.zip](https://github.com/ipcjs/wechat-emoji-dict/releases/download/v1.0.0/wechat-emoji-gboard-flypy.zip)
+- GBoard(全拼): [wechat-emoji-gboard-pinyin.zip](https://github.com/ipcjs/wechat-emoji-dict/releases/download/v1.1.0/wechat-emoji-gboard-pinyin.zip)
+- GBoard(小鹤双拼): [wechat-emoji-gboard-flypy.zip](https://github.com/ipcjs/wechat-emoji-dict/releases/download/v1.1.0/wechat-emoji-gboard-flypy.zip)
+- Rime/小狼毫: [wechat-emoji.dict.yaml](https://github.com/ipcjs/wechat-emoji-dict/releases/download/v1.1.0/wechat-emoji.dict.yaml)
 
-### 预览
+## 图示
 
-![preview](resources/preview.jpg)
+![Rime预览](resources/preview-rime.jpg)
 
-### Emoji列表
+![GBoard预览](resources/preview.jpg)
 
-![all-emoji](resources/all-emoji.jpg)
+![所有Emoji](resources/all-emoji.jpg)
--- main.py
+++ main.py
@@ -6,29 +6,42 @@ import re
 _RE_ENGLISH = re.compile(r'\w+', re.ASCII)
 
 
-def kanji_to_pinyin(kanji: str, py_type='pinyin') -> str:
+def kanji_to_pinyin(kanji: str, py_type='pinyin'):
     if _RE_ENGLISH.fullmatch(kanji):
         # 纯英文字母, 转小写就行
-        return kanji.lower()
-    py = lazy_pinyin(kanji, style=Style.NORMAL)
-    if py_type == 'flypy':
-        py = list(map(lambda s: flypy.todouble(s), py))
-    return ''.join(py)
+        return ([kanji.lower()], True)
+    return (lazy_pinyin(kanji, style=Style.NORMAL), False)
 
-
-def save_to_gboard(list, py_type='pinyin', lang='zh-CN'):
+def save_to_gboard(_list, py_type='pinyin', lang='zh-CN'):
     base_path = f'out/wechat-emoji-gboard-{py_type}'
     dict_file = base_path+'.txt'
     zip_file = base_path + '.zip'
 
     with open(dict_file, 'w', encoding='utf-8') as f:
         f.write('# Gboard Dictionary version:1\n')
-        for item in list:
-            f.write('\t'.join((item[py_type], item['word'], lang)) + '\n')
+        for item in _list:
+            py = item['pinyin']
+            if py_type == 'flypy' and not item['is_english']:
+                py = list(map(lambda s: flypy.todouble(s), py))
+            f.write('\t'.join((''.join(py), item['word'], lang)) + '\n')
     # archive
     with ZipFile(zip_file, 'w') as f:
         f.write(dict_file, 'dictionary.txt')
 
+def save_to_rime(list):
+    with open('out/wechat-emoji.dict.yaml', 'w', encoding='utf-8') as f:
+        f.write('''# wechat-emoji
+---
+name: wechat-emoji
+version: "0.1"
+sort: by_weight
+...
+''')
+        for item in list:
+            if item['is_english']:
+                print(f'skip: {item}')
+            else:
+                f.write(item['word'] + '\t' + ' '.join(item['pinyin']) + '\n')
 
 def main():
     list = []
@@ -40,11 +53,13 @@ def main():
             if len(segments) > 1:
                 kanji_list.extend(segments[1].split(','))
             for kanji in kanji_list:
+                pinyin, is_english = kanji_to_pinyin(kanji)
                 list.append({
                     'word': word,
-                    'pinyin': kanji_to_pinyin(kanji),
-                    'flypy': kanji_to_pinyin(kanji, py_type='flypy'),
+                    'pinyin': pinyin,
+                    'is_english': is_english,
                 })
+    save_to_rime(list)
     save_to_gboard(list)
     save_to_gboard(list, py_type='flypy')
 
